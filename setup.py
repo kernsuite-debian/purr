53439e89210f3878dc8f800d97c1b244d41046c4
Gijs Molenaar <gijs@pythonic.nl>
2019-01-08 16:09:49 +0200

    New upstream version 1.4.0

--- setup.py
+++ setup.py
@@ -1,34 +1,16 @@
 #!/usr/bin/env python
 
-import os
-from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
+from setuptools import setup, find_packages
 
-data_files = []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
+from Kittens import __version__
 
-# collect a list of all non python files
-for dirpath, dirnames, filenames in os.walk('Purr'):
-    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
-    if filenames and '__init__.py' not in filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-# Tell distutils not to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-        scheme['data'] = scheme['purelib']
-
-setup(name='purr',
-      version='1.3.0',
-      description='Data reduction logging tool, Useful for remembering reductions',
+setup(name='astro-kittens',
+      version=__version__,
+      description='Collection of Python utility functions for purr, tigger, meqtrees and others',
       author='Oleg Smirnov',
       author_email='Oleg Smirnov <osmirnov@gmail.com>',
-      url='https://github.com/ska-sa/purr',
-      packages=['Purr', 'Purr/Plugins', 'Purr/Plugins/local_pychart', 'Purr/Plugins/local_pychart/afm'],
-      requires=['kittens', 'PyQt4', 'PIL', 'scipy', 'pyfits'],
-      scripts=['Purr/purr.py', 'Purr/purr'],
-      data_files=data_files,
+      url='https://github.com/ska-sa/kittens',
+      packages=find_packages(),
+      install_requires=['configparser', 'astropy'],
      )
+
